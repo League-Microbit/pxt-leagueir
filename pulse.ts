e1f82b401a40886f08c72cd885519b78e5599576
Eric Busboom <eric@busboom.org>
2025-07-20 12:48:16 -0700

    Refactor pulse generation functions to separate CPP and TypeScript implementations

--- pulse.ts
+++ pulse.ts
@@ -11,16 +11,39 @@ namespace leaguepulse {
     * @param count number of pulses to generate
     * @param delay delay in microseconds for each high and low state
     */
-    //% blockId="leaguepulse_pulse" 
-    //% block="generate %count pulses on pin %pin with %delay μs delay"
+    //% blockId="leaguepulse_pulse_c" 
+    //% block="generate %count pulses on pin %pin with %delay μs delay, implemented in CPP"
     //% weight=100
     //% pin.fieldEditor="gridpicker" pin.fieldOptions.columns=4
     //% pin.fieldOptions.tooltips="false" pin.fieldOptions.width="300"
     //% delay.min=1 delay.max=65535 delay.defl=1000
     //% count.min=1 count.max=1000 count.defl=10
     //% group="Pulse Generation"
-    export function generatePulses(pin: DigitalPin, count: number, delay: number): void {
+    export function generatePulsesCpp(pin: DigitalPin, count: number, delay: number): void {
         pulse(pin as number, delay, count)
+    }
+
+    /**
+     * Generate pulses on a digital pin (TypeScript implementation)
+     * @param pin the digital pin to pulse
+     * @param count number of pulses to generate
+     * @param delay delay in microseconds for each high and low state
+     */
+    //% blockId="leaguepulse_pulse_ts" 
+    //% block="generate %count pulses on pin %pin with %delay μs delay, implemented in TS"
+    //% weight=90
+    //% pin.fieldEditor="gridpicker" pin.fieldOptions.columns=4
+    //% pin.fieldOptions.tooltips="false" pin.fieldOptions.width="300"
+    //% delay.min=1 delay.max=65535 delay.defl=1000
+    //% count.min=1 count.max=1000 count.defl=10
+    //% group="Pulse Generation"
+    export function generatePulsesTs(pin: DigitalPin, count: number, delay: number): void {
+        for (let i = 0; i < count; i++) {
+            pins.digitalWritePin(pin, 1)
+            control.waitMicros(delay)
+            pins.digitalWritePin(pin, 0)
+            control.waitMicros(delay)
+        }
     }
 
     /**
