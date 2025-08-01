c609466c2c525fe0a002bb8d86ac9546005ab181
Eric Busboom <eric@busboom.org>
2025-07-31 17:47:09 -0700

    Update IR timing data for radio and beacon on: Add BIT_MARK, HEADER_MARK, HEADER_SPACE, and ONE_SPACE values for improved accuracy.

--- src/ir.ts
+++ src/ir.ts
@@ -8,6 +8,15 @@
     HEADER_MARK      9020   20     8990     9040
     HEADER_SPACE     4500   20     4470     4520
     ONE_SPACE        1670   90     1200     1770
+
+
+* With radio and beacon on:
+
+    BIT_MARK         570   71     427     858
+    HEADER_MARK     9028   59    8908    9135
+    HEADER_SPACE    4491   53    4404    4592
+    ONE_SPACE       1669  105    1196    1837
+
  */
 
 
