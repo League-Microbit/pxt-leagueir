c18ecf14deb53cdd1df96a76a767f1ae16c1ed69
Eric Busboom <eric@busboom.org>
2025-08-02 20:12:33 -0700

    Enhance documentation for sendIrAddressCommand function parameters

--- src/shims.ts
+++ src/shims.ts
@@ -30,6 +30,9 @@ namespace leagueir {
      * @param command 16-bit command
      */
     //% block="Send IR address %address command %command on pin %pin"
+    //% address.min=0 address.max=65535
+    //% command.min=0 command.max=65535
+    //% pin.fieldEditor="gridpicker" pin.fieldOptions.columns=3
     //% shim=leagueir::sendIrAddressCommandCpp
     export function sendIrAddressCommand(pin: DigitalPin, address: number, command: number): void {
         // Simulator implementation - just log the action
